06744d8cf3be7080121b26ba7752d2eb0ca98c75
chichidd <43724160+chichidd@users.noreply.github.com>
2020-03-11 09:17:21 +0100

    Add files via upload

--- dqn_a3c_for_carracing/dqn/actions.py
+++ dqn_a3c_for_carracing/dqn/actions.py
@@ -2,14 +2,12 @@ import math
 import torch
 import random
 
-
 LEFT = [-1.0, 0.0, 0.0]
 RIGHT = [1.0, 0.0, 0.0]
 GAS = [0.0, 1.0, 0.0]
 BRAKE = [0.0, 0.0, 1.0]
-
-ACTIONS = [LEFT, RIGHT, GAS, BRAKE]
-
+NOTHING = [0.0, 0.0, 0.0]
+ACTIONS = [LEFT, RIGHT, GAS, BRAKE]#, NOTHING]
 
 def get_action_space():
     return len(ACTIONS)
